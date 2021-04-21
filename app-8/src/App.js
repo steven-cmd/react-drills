97725e5b39e2c0605b234757b13a8959472d86f8
Steven Clark <stevenclark@steb.localdomain>
2021-04-21 16:29:08 -0600

    finished app8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fellowship: "",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://the-one-api.dev/v2/book/5cf5805fb53e011a64671582/chapter`)
+      .then((res) => {
+        this.setState({ fellowship: res.data });
+      });
+  }
+
+  render() {
+    let chapters = [];
+    {
+      for (let key in this.state.fellowship.docs) {
+        chapters.push(this.state.fellowship.docs[key].chapterName);
+      }
+    }
+
+    return (
+      <div className="App">
+        <h1>Chapters of The Fellowship of the Ring</h1>
+        {chapters.map((chapter, i) => (
+          <h2 key={i}>{chapter}</h2>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
